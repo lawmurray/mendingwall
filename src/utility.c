7e34f1ccb187f792035de9f8fa5c0e6c4dc446b3
Lawrence Murray <lawrence@indii.org>
2025-01-03 22:09:45 +0700

    Added utility function for finding descendant widget by name.

---
+++ src/utility.c
@@ -0,0 +1,29 @@
+#include <utility.h>
+
+GtkWidget* find_descendant(GtkWidget* widget, const char* name) {
+  GtkWidget* w = widget;
+  while (true) {
+    if (g_strcmp0(gtk_widget_get_name(w), name) == 0) {
+      return w;
+    }
+
+    GtkWidget* child = gtk_widget_get_first_child(w);
+    if (child) {
+      w = child;
+      continue;
+    }
+
+    GtkWidget* next_sibling = gtk_widget_get_next_sibling(w);
+    while (!next_sibling && w != widget) {
+      w = gtk_widget_get_parent(w);
+      next_sibling = gtk_widget_get_next_sibling(w);
+    }
+    if (next_sibling) {
+      w = next_sibling;
+      continue;
+    } else {
+      return NULL;
+    }
+  }
+}
+
---
+++ src/utility.h
@@ -0,0 +1,10 @@
+#include <gtk/gtk.h>
+
+/**
+ * Find descendant of a widget by name.
+ *
+ * @param widget Widget.
+ * @param name Name.
+ */
+GtkWidget* find_descendant(GtkWidget* widget, const char* name);
+
