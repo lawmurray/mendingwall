516630106f2474f82a2213bde0b5017157b972eb
Lawrence Murray <lawrence@indii.org>
2025-01-10 16:30:46 +0700

    Added command-line options to mendingwall-themes and enabled main save/restore to start.

--- src/mendingwall-themes.c
+++ src/mendingwall-themes.c
@@ -95,59 +95,105 @@ void deactivate(GSettings* settings, gchar* key, GMainLoop* loop) {
 }
 
 int main(int argc, char* argv[]) {
+  /* command-line arguments */
+  gboolean save = FALSE, restore = FALSE, watch = FALSE;
+  GOptionEntry options[] = {
+    { "save", 0, 0, G_OPTION_ARG_NONE, &save, "Save current configuration.", NULL },
+    { "restore", 0, 0, G_OPTION_ARG_NONE, &restore, "Restore previously-saved configuration.", NULL },
+    { "watch", 0, 0, G_OPTION_ARG_NONE, &watch, "Continue to watch for changes and save", NULL },
+    G_OPTION_ENTRY_NULL
+  };
+  GError* error = NULL;
+  GOptionContext* context = g_option_context_new ("- protect themes of current desktop environment");
+  g_option_context_set_description(context, "For more information see https://mendingwall.org");
+  g_option_context_add_main_entries(context, options, GETTEXT_PACKAGE);
+  if (!g_option_context_parse(context, &argc, &argv, &error)) {
+    g_print("Error: %s\n", error->message);
+    exit(1);
+  }
+  if (!save && !restore) {
+    g_print("Error: One of --save or --restore must be used\n");
+    exit(1);
+  }
+  if (save && restore) {
+    g_print("Error: Only one of --save or --restore must be used\n");
+    exit(1);
+  }
+
   /* settings */
   g_autoptr(GSettings) global = g_settings_new("org.indii.mendingwall");
 
-  /* check if feature is enabled */
+  /* exit early if feature not enabled */
   if (!g_settings_get_boolean(global, "themes")) {
     return 0;
   }
 
-  /* determine desktop environment */
+  /* desktop environment */
   const gchar* desktop = g_getenv("XDG_CURRENT_DESKTOP");
   if (!desktop) {
-    g_error("environment variable XDG_CURRENT_DESKTOP is not set");
+    g_print("Error: Environment variable XDG_CURRENT_DESKTOP is not set");
+    exit(1);
   }
 
-  /* process config */
+  /* config */
   g_autoptr(GKeyFile) config = g_key_file_new();
   if (!g_key_file_load_from_data_dirs(config, "mendingwall/themes.conf", NULL, G_KEY_FILE_NONE, NULL)) {
-    g_error("mendingwall/themes.conf file not found");
+    g_print("Error: configuration file mendingwall/themes.conf not found");
+    exit(1);
   }
   if (!g_key_file_has_group(config, desktop)) {
-    g_error("desktop environment %s not found in mendingwall/themes.conf", desktop);
+    g_print("Error: desktop environment %s not supported", desktop);
+    exit(1);
   }
 
-  /* monitor */
+  /* save/restore/monitor */
   g_autoptr(GPtrArray) settings = g_ptr_array_new_with_free_func(g_object_unref);
   g_autoptr(GPtrArray) files = g_ptr_array_new_with_free_func(g_object_unref);
   g_autoptr(GPtrArray) monitors = g_ptr_array_new_with_free_func(g_object_unref);
 
-  /* monitor settings schemas */
+  /* settings */
   gchar** schemas = g_key_file_get_string_list(config, desktop, "GSettings", NULL, NULL);
   for (gchar** schema = schemas; *schema; ++schema) {
     GSettings* setting = g_settings_new(*schema);
-    g_signal_connect(setting, "change-event", G_CALLBACK(changed_settings), NULL);
+    if (save) {
+      save_settings(setting);
+    } else if (restore) {
+      restore_settings(setting);
+    }
+    if (watch) {
+      g_signal_connect(setting, "change-event", G_CALLBACK(changed_settings), NULL);
+    }
     g_ptr_array_add(settings, setting);
   }
   g_strfreev(schemas);
 
-  /* monitor files */
+  /* files */
   gchar** paths = g_key_file_get_string_list(config, desktop, "ConfigFiles", NULL, NULL);
   for (gchar** path = paths; *path; ++path) {
     g_autofree char* filename = g_build_filename(g_get_user_config_dir(), *path, NULL);
     GFile* file = g_file_new_for_path(filename);
-    GFileMonitor* monitor = g_file_monitor_file(file, G_FILE_MONITOR_NONE, NULL, NULL);
-    g_signal_connect(monitor, "changed", G_CALLBACK(changed_file), NULL);
-    g_ptr_array_add(files, file);
-    g_ptr_array_add(monitors, monitor);
+    if (save) {
+      save_file(file);
+    } else if (restore) {
+      restore_file(file);
+    }
+    if (watch) {
+      GFileMonitor* monitor = g_file_monitor_file(file, G_FILE_MONITOR_NONE, NULL, NULL);
+      g_signal_connect(monitor, "changed", G_CALLBACK(changed_file), NULL);
+      g_ptr_array_add(files, file);
+      g_ptr_array_add(monitors, monitor);
+    }
   }
   g_strfreev(paths);
 
-  /* start main loop */
-  g_autoptr(GMainLoop) loop = g_main_loop_new(NULL, FALSE);
-  GApplication* app = g_application_new("org.indii.mendingwall-themes", G_APPLICATION_DEFAULT_FLAGS);
-  g_signal_connect(app, "activate", G_CALLBACK(activate), loop);
-  g_signal_connect(global, "changed", G_CALLBACK(deactivate), loop);
-  return g_application_run(G_APPLICATION(app), argc, argv);
+  /* main loop */
+  if (watch) {
+    g_autoptr(GMainLoop) loop = g_main_loop_new(NULL, FALSE);
+    GApplication* app = g_application_new("org.indii.mendingwall-themes", G_APPLICATION_DEFAULT_FLAGS);
+    g_signal_connect(app, "activate", G_CALLBACK(activate), loop);
+    g_signal_connect(global, "changed", G_CALLBACK(deactivate), loop);
+    return g_application_run(G_APPLICATION(app), argc, argv);
+  } else {
+    return 0;
+  }
 }
