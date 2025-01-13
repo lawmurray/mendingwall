b9addc1aec00a52ffd3b1076c6c784005a27b399
Lawrence Murray <lawrence@indii.org>
2025-01-13 13:25:08 +0700

    Delete config files and reset settings to default if no save exists; the intention here is to clear the user's home directory for the first launch of a new desktop environment.

--- src/mendingwall-themes-restore.c
+++ src/mendingwall-themes-restore.c
@@ -6,39 +6,56 @@
 #include <gio/gsettingsbackend.h>
 
 static void restore_settings(GSettings* to) {
+  /* schema */
   GSettingsSchema* schema = NULL;
   g_object_get(to, "settings-schema", &schema, NULL);
 
+  /* save file */
   const gchar* id = g_settings_schema_get_id(schema);
   const gchar* desktop = g_getenv("XDG_CURRENT_DESKTOP");
   g_autofree gchar* filename = g_strconcat(g_get_user_data_dir(), "/mendingwall/save/", desktop, ".gsettings", NULL);
-
   g_autoptr(GFile) file = g_file_new_for_path(filename);
+
+  gchar** keys = g_settings_schema_list_keys(schema);
   if (g_file_query_exists(file, NULL)) {
+    /* if the save file exists, restore settings */
     g_autoptr(GSettingsBackend) backend = g_keyfile_settings_backend_new(filename, "/", NULL);
     g_autoptr(GSettings) from = g_settings_new_with_backend(id, backend);
-
-    gchar** keys = g_settings_schema_list_keys(schema);
     for (gchar** key = keys; *key; ++key) {
       g_autoptr(GVariant) value = g_settings_get_value(from, *key);
       g_settings_set_value(to, *key, value);
     }
-    g_strfreev(keys);
+  } else {
+    /* if the save file does not exist, restore settings to their defaults;
+     * this is deliberate, it means that when starting a new desktop
+     * environment for the first time after running some other desktop
+     * environment, existing settings are reset, the user's settings look
+     * pristine, and the new desktop environment performs its default initial
+     * setup */
+    for (gchar** key = keys; *key; ++key) {
+      g_settings_reset(to, *key);
+    }
   }
+  g_strfreev(keys);
 }
 
 static void restore_file(GFile* to) {
   const gchar* desktop = g_getenv("XDG_CURRENT_DESKTOP");
   g_autoptr(GFile) save = g_file_new_build_filename(g_get_user_data_dir(), "mendingwall", "save", desktop, NULL);
-  if (g_file_query_exists(save, NULL)) {
-    g_autoptr(GFile) config = g_file_new_for_path(g_get_user_config_dir());
-    g_autofree char* rel = g_file_get_relative_path(config, to);
-    g_autoptr(GFile) from = g_file_new_build_filename(g_get_user_data_dir(), "mendingwall", "save", desktop, rel, NULL);
-    if (g_file_query_exists(from, NULL)) {
-      g_file_copy(from, to, G_FILE_COPY_OVERWRITE|G_FILE_COPY_ALL_METADATA, NULL, NULL, NULL, NULL);
-    } else {
-      g_file_delete(to, NULL, NULL);
-    }
+  g_autoptr(GFile) config = g_file_new_for_path(g_get_user_config_dir());
+  g_autofree char* rel = g_file_get_relative_path(config, to);
+  g_autoptr(GFile) from = g_file_new_build_filename(g_get_user_data_dir(), "mendingwall", "save", desktop, rel, NULL);
+  if (g_file_query_exists(from, NULL)) {
+    /* restore the file */
+    g_file_copy(from, to, G_FILE_COPY_OVERWRITE|G_FILE_COPY_ALL_METADATA, NULL, NULL, NULL, NULL);
+  } else {
+    /* delete the file; this happens either because the file is not part of
+     * the save, or there is no save; the latter occurs when starting a new
+     * desktop environment for the first time after running some other desktop
+     * environment, and by deleting all config files, the user's home
+     * directory looks pristine, and the new desktop environment performs its
+     * default initial setup */
+    g_file_delete(to, NULL, NULL);
   }
 }
 
