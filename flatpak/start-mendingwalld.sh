7817da502af7f69632583234ee26933bdbf8b8e3
Lawrence Murray <lawrence@indii.org>
2025-02-07 18:28:13 +0700

    Hard-coded host XDG_DATA_DIRS in order to remove need for --talk-name=org.freedesktop.Flatpak sandbox permissions, which create ominous warning. Moved build-aux/ subdirectory to flatpak/.

---
+++ flatpak/start-mendingwalld.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+# Need access to the host XDG_DATA_DIRS to see which applications are
+# installed to tidy menus accordingly. These paths are hard-coded, which is
+# unfortunate, but the only alternative seems to be calling `flatpak-spawn
+# --host` to get them (which is done by e.g. dconf-editor and Refine), but
+# that requires very broad sandbox permissions that are not otherwise required
+# (--talk-name=org.freedesktop.Flatpak) and results in a warning on Flathub
+# about arbitrary permissions. For the purposes of Mending Wall this
+# hard-coded set ought to be sufficient anyway.
+export XDG_DATA_DIRS="/var/lib/flatpak/exports/share:/var/lib/snapd/desktop:/run/host/usr/local/share:/run/host/usr/share:$XDG_DATA_DIRS"
+export GSETTINGS_BACKEND=dconf
+exec mendingwalld "$@"
