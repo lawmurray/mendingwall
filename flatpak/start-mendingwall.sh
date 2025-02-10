441a0db8e97bc685c966428b95cf2462de9a8270
Lawrence Murray <lawrence@indii.org>
2025-02-10 16:12:54 +0700

    Restored use of start scripts for all but one environment variable, and documented reasoning for these changes.

--- flatpak/start-mendingwall.sh
+++ flatpak/start-mendingwall.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/env sh
 
-# See commentary in start-mendingwalld.sh for this environment variable. While
-# mendingwall does not require it directly, but may spawn mendingwalld, which
-# will
-#export XDG_DATA_DIRS="/var/lib/flatpak/exports/share:/var/lib/snapd/desktop:/run/host/usr/local/share:/run/host/usr/share:$XDG_DATA_DIRS"
-#unset XDG_CONFIG_HOME  # i.e. use default
-#export GSETTINGS_BACKEND=dconf
+# See commentary in start-mendingwalld.sh for these environment variables.
+# While mendingwall does not actually need them, it may spawn mendingwalld,
+# which does.
+
+export XDG_DATA_DIRS="/var/lib/flatpak/exports/share:/var/lib/snapd/desktop:/run/host/usr/local/share:/run/host/usr/share:$XDG_DATA_DIRS"
+unset XDG_CONFIG_HOME
+
 exec mendingwall "$@"
