9d6b037eaae02b19ccbd2c467cd3b3f1d2db5c9e
Lawrence Murray <lawrence@indii.org>
2025-02-10 10:31:55 +0700

    Revert to spawn rather than dbus for launching mendingwalld from mendingwall, due to Flatpak issues.

--- flatpak/start-mendingwall.sh
+++ flatpak/start-mendingwall.sh
@@ -1,4 +1,8 @@
 #!/usr/bin/env sh
 
+# See commentary in start-mendingwalld.sh for this environment variable. While
+# mendingwall does not require it directly, but may spawn mendingwalld, which
+# will
+export XDG_DATA_DIRS="/var/lib/flatpak/exports/share:/var/lib/snapd/desktop:/run/host/usr/local/share:/run/host/usr/share:$XDG_DATA_DIRS"
 export GSETTINGS_BACKEND=dconf
 exec mendingwall "$@"
