bd371a262808d6aaaac7b3e9cacfc0081005c0a8
Lawrence Murray <lawrence@indii.org>
2025-02-11 12:37:19 +0700

    Added Flatpak tests and updated sandbox permissions, etc, accordingly.

--- flatpak/start-mendingwalld.sh
+++ flatpak/start-mendingwalld.sh
@@ -1,13 +1,42 @@
 #!/usr/bin/env bash
 
-# Need access to the host XDG_DATA_DIRS to see which applications are
-# installed to tidy menus accordingly. These paths are hard-coded, which is
-# unfortunate, but the only alternative seems to be calling `flatpak-spawn
-# --host` to get them (which is done by e.g. dconf-editor and Refine), but
-# that requires very broad sandbox permissions that are not otherwise required
-# (--talk-name=org.freedesktop.Flatpak) and results in a warning on Flathub
-# about arbitrary permissions. For the purposes of Mending Wall this
-# hard-coded set ought to be sufficient anyway.
+# Access is needed to host data directories to see which applications are
+# installed for the purposes of tidying menus. Outside of Flatpak, these are
+# given by XDG_DATA_DIRS; inside of Flatpak they are overridden. We need to
+# get them back.
+#
+# There are a couple of ways to achieve this:
+#
+#   1. They can be hard-coded.
+#   2. They can be acquired with `flatpak-spawn --host`, but this requires the
+#      --talk-name=org.freedesktop.Flatpak sandbox permission, which is very
+#      broad, and results in a Flathub warning about the app's ability to
+#      acquire arbitrary permissions.
+#
+# The former is chosen here to keep sandbox permissions tight. The latter is
+# chosen by other apps such as dconf-editor and Refine, which offer a good
+# reference if this needs to change.
+
 export XDG_DATA_DIRS="/var/lib/flatpak/exports/share:/var/lib/snapd/desktop:/run/host/usr/local/share:/run/host/usr/share:$XDG_DATA_DIRS"
-export GSETTINGS_BACKEND=dconf
+
+
+# Access is needed to the configuration files of desktop environments for the
+# purposes of saving and restoring themes. Outside of Flatpak, the
+# configuration directory is given by XDG_CONFIG_HOME, or if not set the
+# default ~/.config; inside of Flatpak this is overridden to a directory used
+# by the current app only, typically somewhere under ~/.app. We need to get it
+# back.
+#
+# We don't need any config files of our own in there, as all configuration is
+# kept in GSettings with the dconf backend for storage. So we unset the
+# XDG_CONFIG_HOME as set by Flatpak, and use the default ~/.config to find the
+# configuration files of desktop environments. More precisely, code can then
+# use the convenience function g_get_user_config_dir() from GLib to get the
+# right directory.
+
+unset XDG_CONFIG_HOME
+
+
+# Phew, fixed, we can launch.
+
 exec mendingwalld "$@"
--- src/mendingwallapplication.c
+++ src/mendingwallapplication.c
@@ -34,27 +34,26 @@ static void launch_daemon(MendingwallApplication* self) {
   gboolean menus = g_settings_get_boolean(self->global, "menus");
 
   if (themes || menus) {
-    /* launch daemon; fine if already running, new instance will quit */
-    g_settings_sync();  // ensure current settings visible in new process
-    g_dbus_connection_call(
-      g_application_get_dbus_connection(G_APPLICATION(self)),
-      "org.indii.mendingwall.watch",
-      "/org/indii/mendingwall/watch",
-      "org.freedesktop.Application",
-      "Activate",
-      g_variant_new_parsed("({'test': <1>}, )"),
-      NULL,
-      G_DBUS_CALL_FLAGS_NONE,
-      -1,
-      NULL,
-      NULL,
-      NULL
-    );
+    /* ensure that current settings will be visible in new processes */
+    g_settings_sync();
 
-    /* alternatively, can launch with g_spawn_async(), but dbus preferred for
-      * a consistent environment */
-    //static const gchar* argv[] = { "mendingwall", "--watch" };
-    //g_spawn_async(NULL, (gchar**)argv, NULL, G_SPAWN_SEARCH_PATH, NULL, NULL, NULL, NULL);
+    /* launch daemon; fine if already running, new instance will quit */
+    g_dbus_connection_call(
+        g_application_get_dbus_connection(G_APPLICATION(self)),
+        "org.indii.mendingwall.watch",
+        "/org/indii/mendingwall/watch",
+        "org.freedesktop.Application",
+        "Activate",
+        g_variant_new_parsed("({'test': <1>}, )"),
+        NULL,
+        G_DBUS_CALL_FLAGS_NONE,
+        -1,
+        NULL,
+        NULL,
+        NULL
+    );
+    // static const gchar* argv[] = { "mendingwalld", "--watch", NULL };
+    // g_spawn_async(NULL, (gchar**)argv, NULL, G_SPAWN_SEARCH_PATH, NULL, NULL, NULL, NULL);
   }
 }
 
